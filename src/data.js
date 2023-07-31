1eb6af4e257e3ff9552a6d9eb89523b988efc984
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-07-31 17:09:41 +0300

    reafact

---
+++ src/data.js
@@ -0,0 +1,35 @@
+export const questions = [
+  {
+    question: "Что такое DOM в JavaScript?",
+    options: [
+      "Document Object Model",
+      "Data Object Model",
+      "Dynamic Object Model",
+    ],
+    answer: "Document Object Model",
+  },
+  {
+    question: "Как объявить переменную в JavaScript?",
+    options: ["var", "int", "string"],
+    answer: "var",
+  },
+  {
+    question: "Что такое closure (замыкание) в JavaScript?",
+    options: [
+      "Захватывание значений внешних переменных",
+      "Безопасность данных",
+      "Работа с сетью",
+    ],
+    answer: "Захватывание значений внешних переменных",
+  },
+  {
+    question: "Как проверить что число целочисленное?",
+    options: [
+      "Умножить на 10",
+      "Разделить на 10",
+      "Разделить с остатком (%) на 1 и если результат деления будет равен 0, то значит число целое и наоборот",
+    ],
+    answer:
+      "Разделить с остатком (%) на 1 и если результат деления будет равен 0, то значит число целое и наоборот",
+  },
+];
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { questions } from "./data";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <App questions={questions} />
   </React.StrictMode>
 );
