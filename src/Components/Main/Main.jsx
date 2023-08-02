41743f22ec183c9eb8c50486fb4c6c95a0ff6ecd
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-08-02 13:28:21 +0300

    upd

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,13 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useFormik } from "formik";
+import axios from "axios";
 import styles from "./Main.module.scss";
 import Question from "../Question/Question";
 import EndTest from "../EndTest/EndTest";
 
-const Main = ({ questions }) => {
+const Main = () => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [correctAnswers, setCorrectAnswers] = useState(0);
   const [finished, setFinished] = useState(false);
+  const [questions, setQuestions] = useState([]);
+
+  const API = "https://alex-pacemaker.ru/quiz";
+
+  useEffect(() => {
+    (async () => {
+      await axios.get(API).then((res) => {
+        setQuestions(res.data);
+        console.log(questions);
+      });
+    })();
+  }, []);
 
   const handleNext = () => {
     setCurrentQuestion(currentQuestion + 1);
@@ -52,7 +65,7 @@ const Main = ({ questions }) => {
             Вопрос {currentQuestion + 1} из {questions.length}
           </h2>
           <Question
-            question={currentQuestionObj.question}
+            questions={currentQuestionObj.question}
             options={currentQuestionObj.options}
             setFieldValue={formik.setFieldValue}
           />
@@ -76,3 +89,375 @@ const Main = ({ questions }) => {
 };
 
 export default Main;
+
+// // import React, { useEffect, useState } from "react";
+// // import { useFormik } from "formik";
+// // import axios from "axios";
+// // import styles from "./Main.module.scss";
+// // import Question from "../Question/Question";
+// // import EndTest from "../EndTest/EndTest";
+
+// // const Main = () => {
+// //   const [currentQuestion, setCurrentQuestion] = useState(0);
+// //   const [correctAnswers, setCorrectAnswers] = useState(0);
+// //   const [finished, setFinished] = useState(false);
+// //   const [questions, setQuestions] = useState([]);
+
+// //   const API = "https://alex-pacemaker.ru/quiz";
+
+// //   useEffect(() => {
+// //     (async () => {
+// //       const response = await axios.get(API);
+// //       setQuestions(response.data);
+// //     })();
+// //   }, []);
+
+// //   const handleNext = () => {
+// //     setCurrentQuestion(currentQuestion + 1);
+// //   };
+
+// //   const checkAnswer = (values) => {
+// //     return values.answer === questions[currentQuestion].answer;
+// //   };
+
+// //   const handleSubmit = (values, { setSubmitting }) => {
+// //     if (checkAnswer(values)) {
+// //       setCorrectAnswers(correctAnswers + 1);
+// //     }
+// //     if (currentQuestion === questions.length - 1) {
+// //       setFinished(true);
+// //     } else {
+// //       handleNext();
+// //     }
+// //     setSubmitting(false);
+// //   };
+
+// //   const resetTest = () => {
+// //     setCurrentQuestion(0);
+// //     setCorrectAnswers(0);
+// //     setFinished(false);
+// //   };
+
+// //   const formik = useFormik({
+// //     initialValues: {
+// //       answer: "",
+// //     },
+// //     onSubmit: handleSubmit,
+// //   });
+
+// //   const currentQuestionObj = questions[currentQuestion];
+
+// //   console.log(currentQuestionObj);
+
+// //   return (
+// //     <div className={styles.Main}>
+// //       {!finished ? (
+// //         <>
+// //           <h2 className={styles.title}>
+// //             Вопрос {currentQuestion + 1} из {questions.length}
+// //           </h2>
+// //           <Question
+// //             question={currentQuestionObj.question}
+// //             options={currentQuestionObj.options}
+// //             setFieldValue={formik.setFieldValue}
+// //           />
+// //           <button
+// //             className={styles.submit}
+// //             type='submit'
+// //             onClick={formik.handleSubmit}
+// //           >
+// //             Ответить
+// //           </button>
+// //         </>
+// //       ) : (
+// //         <EndTest
+// //           correctAnswers={correctAnswers}
+// //           questionsLength={questions.length}
+// //           resetTest={resetTest}
+// //         />
+// //       )}
+// //     </div>
+// //   );
+// // };
+
+// // export default Main;
+
+// // import React, { useEffect, useState } from "react";
+// // import { useFormik } from "formik";
+// // import axios from "axios";
+// // import styles from "./Main.module.scss";
+// // import Question from "../Question/Question";
+// // import EndTest from "../EndTest/EndTest";
+
+// // const Main = () => {
+// //   const [currentQuestion, setCurrentQuestion] = useState(0);
+// //   const [correctAnswers, setCorrectAnswers] = useState(0);
+// //   const [finished, setFinished] = useState(false);
+// //   const [questions, setQuestions] = useState([]);
+
+// //   const API = "https://alex-pacemaker.ru/quiz";
+
+// //   useEffect(() => {
+// //     const fetchData = async () => {
+// //       const response = await axios.get(API);
+// //       setQuestions(response.data);
+// //     };
+
+// //     fetchData();
+// //   }, []);
+
+// //   const handleNext = () => {
+// //     setCurrentQuestion(currentQuestion + 1);
+// //   };
+
+// //   const checkAnswer = (values) => {
+// //     return values.answer === questions[currentQuestion].answer;
+// //   };
+
+// //   const handleSubmit = (values, { setSubmitting }) => {
+// //     if (checkAnswer(values)) {
+// //       setCorrectAnswers(correctAnswers + 1);
+// //     }
+// //     if (currentQuestion === questions.length - 1) {
+// //       setFinished(true);
+// //     } else {
+// //       handleNext();
+// //     }
+// //     setSubmitting(false);
+// //   };
+
+// //   const resetTest = () => {
+// //     setCurrentQuestion(0);
+// //     setCorrectAnswers(0);
+// //     setFinished(false);
+// //   };
+
+// //   const formik = useFormik({
+// //     initialValues: {
+// //       answer: "",
+// //     },
+// //     onSubmit: handleSubmit,
+// //   });
+
+// //   const currentQuestionObj = questions[currentQuestion];
+
+// //   console.log(currentQuestionObj);
+
+// //   return (
+// //     <div className={styles.Main}>
+// //       {!finished ? (
+// //         <>
+// //           <h2 className={styles.title}>
+// //             Вопрос {currentQuestion + 1} из {questions.length}
+// //           </h2>
+// //           <Question
+// //             question={currentQuestionObj?.question} // использование оператора ? для проверки наличия свойства
+// //             options={currentQuestionObj?.options} // использование оператора ? для проверки наличия свойства
+// //             setFieldValue={formik.setFieldValue}
+// //           />
+// //           <button
+// //             className={styles.submit}
+// //             type='submit'
+// //             onClickformik.handleSubmit}
+// //           >
+// //             Ответить
+// //           </button>
+// //         </>
+// //       ) : (
+// //         <EndTest
+// //           correctAnswers={correctAnswers}
+// //           questionsLength={questions.length}
+// //           resetTest={resetTest}
+// //         />
+// //       )}
+// //     </div>
+// //   );
+// // };
+
+// // export default Main;
+
+// // import React, { useEffect, useState } from "react";
+// // import { useFormik } from "formik";
+// // import axios from "axios";
+// // import styles from "./Main.module.scss";
+// // import Question from "../Question/Question";
+// // import EndTest from "../EndTest/EndTest";
+
+// // const Main = () => {
+// //   const [currentQuestion, setCurrentQuestion] = useState(0);
+// //   const [correctAnswers, setCorrectAnswers] = useState(0);
+// //   const [finished, setFinished] = useState(false);
+// //   const [questions, setQuestions] = useState([]);
+
+// //   const API = "https://alex-pacemaker.ru/quiz";
+
+// //   useEffect(() => {
+// //     const fetchData = async () => {
+// //       const response = await axios.get(API);
+// //       setQuestions(response.data);
+// //     };
+
+// //     fetchData();
+// //   }, []);
+
+// //   const handleNext = () => {
+// //     setCurrentQuestion(currentQuestion + 1);
+// //   };
+
+// //   const checkAnswer = (values) => {
+// //     return values.answer === questions[currentQuestion].answer;
+// //   };
+
+// //   const handleSubmit = (values, { setSubmitting }) => {
+// //     if (checkAnswer(values)) {
+// //       setCorrectAnswers(correctAnswers + 1);
+// //     }
+// //     if (currentQuestion === questions.length - 1) {
+// //       setFinished(true);
+// //     } else {
+// //       handleNext();
+// //     }
+// //     setSubmitting(false);
+// //   };
+
+// //   const resetTest = () => {
+// //     setCurrentQuestion(0);
+// //     setCorrectAnswers(0);
+// //     setFinished(false);
+// //   };
+
+// //   const formik = useFormik({
+// //     initialValues: {
+// //       answer: "",
+// //     },
+// //     onSubmit: handleSubmit,
+// //   });
+
+// //   const currentQuestionObj = questions[currentQuestion];
+
+// //   return (
+// //     <div className={styles.Main}>
+// //       {!finished ? (
+// //         <>
+// //           <h2 className={styles.title}>
+// //             Вопрос {currentQuestion + 1} из {questions.length}
+// //           </h2>
+// //           <Question
+// //             question={currentQuestionObj?.question}
+// //             options={currentQuestionObj?.options}
+// //             setFieldValue={formik.setFieldValue}
+// //           />
+// //           <button
+// //             className={styles.submit}
+// //             type='submit'
+// //             onClick={formik.handleSubmit}
+// //           >
+// //             Ответить
+// //           </button>
+// //         </>
+// //       ) : (
+// //         <EndTest
+// //           correctAnswers={correctAnswers}
+// //           questionsLength={questions.length}
+// //           resetTest={resetTest}
+// //         />
+// //       )}
+// //     </div>
+// //   );
+// // };
+
+// // export default Main;
+
+// import React, { useEffect, useState } from "react";
+// import { useFormik } from "formik";
+// import axios from "axios";
+// import styles from "./Main.module.scss";
+// import Question from "../Question/Question";
+// import EndTest from "../EndTest/EndTest";
+
+// const Main = () => {
+//   const [currentQuestion, setCurrentQuestion] = useState(0);
+//   const [correctAnswers, setCorrectAnswers] = useState(0);
+//   const [finished, setFinished] = useState(false);
+//   const [questions, setQuestions] = useState([]);
+
+//   const API = "https://alex-pacemaker.ru/quiz";
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       const response = await axios.get(API);
+//       setQuestions(response.data);
+//     };
+
+//     fetchData();
+//   }, []);
+
+//   const handleNext = () => {
+//     setCurrentQuestion(currentQuestion + 1);
+//   };
+
+//   const checkAnswer = (values) => {
+//     return values.answer === questions[currentQuestion]?.answer;
+//   };
+
+//   const handleSubmit = (values, { setSubmitting }) => {
+//     if (checkAnswer(values)) {
+//       setCorrectAnswers(correctAnswers + 1);
+//     }
+//     if (currentQuestion === questions.length - 1) {
+//       setFinished(true);
+//     } else {
+//       handleNext();
+//     }
+//     setSubmitting(false);
+//   };
+
+//   const resetTest = () => {
+//     setCurrentQuestion(0);
+//     setCorrectAnswers(0);
+//     setFinished(false);
+//   };
+
+//   const formik = useFormik({
+//     initialValues: {
+//       answer: "",
+//     },
+//     onSubmit: handleSubmit,
+//   });
+
+//   const currentQuestionObj = questions[currentQuestion];
+
+//   console.log(currentQuestionObj);
+
+//   return (
+//     <div className={styles.Main}>
+//       {!finished ? (
+//         <>
+//           <h2 className={styles.title}>
+//             Вопрос {currentQuestion + 1} из {questions.length}
+//           </h2>
+//           <Question
+//             question={currentQuestionObj?.question}
+//             options={currentQuestionObj?.options}
+//             setFieldValue={formik.setFieldValue}
+//           />
+//           <button
+//             className={styles.submit}
+//             type='submit'
+//             onClick={formik.handleSubmit}
+//           >
+//             Ответить
+//           </button>
+//         </>
+//       ) : (
+//         <EndTest
+//           correctAnswers={correctAnswers}
+//           questionsLength={questions.length}
+//           resetTest={resetTest}
+//         />
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Main;
