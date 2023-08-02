16367f009a7c8a228ce913069cf40b0bee70c19e
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-08-02 15:39:49 +0300

    mainslice done

--- src/redux/slices/mainSlice.js
+++ src/redux/slices/mainSlice.js
@@ -10,9 +10,43 @@ const initialState = {
 };
 
 export const mainSlice = createSlice({
-    name:'@main',
-    initialState,
-    reducers:{
-        
-    }
-})
+  name: "@main",
+  initialState,
+  reducers: {
+    setCurrentQuestion(state) {
+      state.currentQuestion += 1;
+    },
+    setCorrectAnswers(state) {
+      state.correctAnswers += 1;
+    },
+    setIncorrectAnswers(state) {
+      state.incorrectAnswers += 1;
+    },
+    setFinished(state, action) {
+      state.finished = action.payload;
+    },
+    setQuestions(state, action) {
+      state.questions = action.payload;
+    },
+    setSelectedAnswer(state, action) {
+      state.selectedAnswer = action.payload;
+    },
+  },
+});
+
+export const {
+  currentQuestion,
+  correctAnswers,
+  incorrectAnswers,
+  finished,
+  questions,
+  selectedAnswer,
+  setCurrentQuestion,
+  setCorrectAnswers,
+  setIncorrectAnswers,
+  setFinished,
+  setQuestions,
+  setSelectedAnswer,
+} = mainSlice.actions;
+
+export default mainSlice.reducer;
